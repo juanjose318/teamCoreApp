c4d8fdba4367a93866585959c6a1ce84850af163
Juan <juanmuno@student.arteveldehs.be>
2021-05-25 19:30:41 +0200

    esqueleto buscar y creaer aliado componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 const routes: Routes = [
     {
         path: 'configAliados',
         loadChildren: './pages/page.module#PageModule'
     }
-  ];
-  
+];
+
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule],
