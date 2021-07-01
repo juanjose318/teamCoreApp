890f9bb171fac22e9de75f25c7a778dfa6654ace
Camilo Alzate <camilo.alzate@carvajal.com>
2021-07-01 14:20:35 -0500

    [TEAMCORE] Agregar funcionalidad para Base 64 en archivos, y uso de estos en los objetos de la interfase MasterFile.

--- src/app/components/upload-master-file/upload-master-file.component.ts
+++ src/app/components/upload-master-file/upload-master-file.component.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { MasterFileService } from 'src/app/services/masterfile/masterfile.service';
+import { base64 } from 'angular-base64/angular-base64';
+import { MasterFile } from 'src/app/models/MasterFile.interface';
 
 @Component({
   selector: 'app-upload-master-file',
@@ -12,10 +14,10 @@ export class UploadMasterFileComponent {
   @ViewChild('file') file: ElementRef;
   fileData: File = null;
 
-  constructor(private http: HttpClient, private masterFileService : MasterFileService) { }
+  constructor(private http: HttpClient, private masterFileService: MasterFileService, private masterFile: MasterFile) { }
 
   ngOnInit() {
-   
+
   }
 
   fileProgress(fileInput: any) {
@@ -23,12 +25,30 @@ export class UploadMasterFileComponent {
   }
 
   onSubmit() {
+    var dataEncode = '';
+    var dataDecode = '';
     const formData = new FormData();
     //formData.append('file', this.fileData);
-    formData.append('file', 
-    this.file.nativeElement.files[0], 
-    this.file.nativeElement.files[0].name);
-    this.masterFileService.uploadFile(formData);
+    formData.append('file',
+      this.file.nativeElement.files[0],
+      this.file.nativeElement.files[0].name);
+    let masterFile: MasterFile;
+    masterFile = {
+      "fileName": this.file.nativeElement.files[0].name,
+      "detail": btoa(this.file.nativeElement.files[0]),
+      "idMasterFile": 0,
+      "idAlliedCompanyConfig": 0,
+      "idAlliedCompanyConfAudit": 0,
+      "userName": "camalzgo",
+      "master": "Producto",
+      "startDateLoad": new Date(Date.now()),
+      "endDateLoad": new Date(Date.now())
+    };
+    //dataEncode = btoa("username:temppass");
+    //console.log(dataEncode);
+    //dataDecode = atob(dataEncode);
+    //console.log(dataDecode);
+    //this.masterFileService.uploadFile(formData);
   }
 
 }
